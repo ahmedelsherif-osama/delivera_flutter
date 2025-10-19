1f811dd34d1481f933ca18bfa3f4293978a7d234
rem <ahmedelsherif.osama@gmail.com>
2025-10-19 12:50:50 +0300

    feat(startNotificationService): in case of already loggedIn user call notificationService.connect

--- lib/features/authentication/presentation/main_layout.dart
+++ lib/features/authentication/presentation/main_layout.dart
@@ -1,8 +1,13 @@
+import 'package:delivera_flutter/features/authentication/data/token_storage.dart';
+import 'package:delivera_flutter/features/authentication/data/user_storage.dart';
 import 'package:delivera_flutter/features/authentication/logic/auth_provider.dart';
+import 'package:delivera_flutter/features/authentication/logic/user_provider.dart';
 import 'package:delivera_flutter/features/authentication/presentation/authentication_layout.dart';
 import 'package:delivera_flutter/features/authentication/presentation/home_page.dart';
 import 'package:delivera_flutter/features/authentication/presentation/login_page.dart';
 import 'package:delivera_flutter/features/authentication/presentation/splash_page.dart';
+import 'package:delivera_flutter/features/notifications/services/notifications_service.dart';
+import 'package:delivera_flutter/features/notifications/services/system_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
@@ -30,6 +35,14 @@ class _MainLayoutState extends ConsumerState<MainLayout> {
     delayForSplash();
   }
 
+  _startNotificationService() async {
+    final token = await TokenStorage().getAccessToken();
+    final user = await ref.read(userStorageProvider).getUser();
+    print("token $token and user ${user!.id} in main layout");
+    final notificationService = ref.read(notificationServiceProvider);
+    await notificationService.connect(token!, user.id);
+  }
+
   @override
   Widget build(BuildContext context) {
     final isLoggedIn = ref.watch(authProvider);
@@ -43,6 +56,7 @@ class _MainLayoutState extends ConsumerState<MainLayout> {
     } else if (isLoggedIn.hasError) {
       body = Center(child: Text(isLoggedIn.error.toString()));
     } else if (isLoggedIn.value == true) {
+      _startNotificationService();
       body = const HomePage();
     } else {
       body = const AuthenticationLayout(); // 👈 clean, no Expanded/Column
